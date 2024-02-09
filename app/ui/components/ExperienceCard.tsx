2a1ead96a2bb64e0223da8225dcc17d4ad3a0f27
Marek Tonder <m.tonder@iceo.co>
2024-02-09 15:02:38 +0100

    Implemented dynamic experience cards + content update

--- app/ui/components/ExperienceCard.tsx
+++ app/ui/components/ExperienceCard.tsx
@@ -1,67 +1,43 @@
 import React from "react";
 import Badge from "./Badge";
 
-type Props = {};
+type Project = {
+  title: string;
+  description: string;
+  responsibilities: string[];
+  techStack: string[];
+};
 
-export default function ExperienceCard({}: Props) {
+type Job = {
+  period: string;
+  company: string;
+  jobTitle: string;
+  projects: Project[];
+};
+
+export default function ExperienceCard({ period, company, jobTitle, projects }: Job) {
   return (
     <article className="flex w-full min-w-[300px] flex-shrink-0 snap-center flex-col items-center space-y-8 overflow-hidden overflow-y-auto rounded-lg bg-[#292929] p-4 md:w-[600px] md:p-10 xl:w-[900px]">
       <div className="w-full px-0 text-left md:px-10">
-        <p className="text-sm">Sep 2021 - Jun 2023</p>
-        <p className="font-bold">ICEO - Venture Builder</p>
-        <h4 className="text-xl font-semibold uppercase text-[#324DD2] md:text-2xl">
-          VP of Frontend Development
-        </h4>
-        <p className="font-semibold">Norion • Fintech/Blockchain • Frontend Department</p>
-        <span className="text-sm">
-          Platform for building complete tokenisation ecosystems. Offers the issuance and sale of
-          token for any asset.
-        </span>
-        <ul className="ml-5 list-disc space-y-1 text-sm">
-          <li>
-            Prepared frontend architecture for complex client-tailored microecosystems concept -
-            domain driven multirepo with components library.
-          </li>
-          <li>
-            Worked as a developer during initial phase of the project - implemented components,
-            tests etc.
-          </li>
-          <li>
-            Conducted 100+ technical interviews in order to build experienced, varied team with
-            proper skills and mindset for project.
-          </li>
-        </ul>
-        <div className="my-1 inline-flex flex-auto flex-wrap overflow-hidden text-nowrap text-sm">
-          <Badge label="React" />
-          <Badge label="MUI" />
-          <Badge label="TypeScript" />
-          <Badge label="Redux Toolkit" />
-          <Badge label="Storybook" />
-          <Badge label="RTL" />
-          <Badge label="Cypress" />
-          <Badge label="Zeroheight" />
-          <Badge label="Design system" />
-        </div>
-        <p className="mt-2 font-semibold">ICEO • Management</p>
-        <span className="text-sm">Member of company management.</span>
-        <ul className="ml-5 list-disc space-y-1 text-sm">
-          <li>
-            Member of company management - responsible for company strategy, development and growth.
-          </li>
-          <li>Frontend representative in company management level meetings.</li>
-          <li>
-            Managed and growth frontend team from business side - structure, evaluations,
-            negotiations, procedures, integrations, onboardings, JIRA.
-          </li>
-        </ul>
-        <div className="my-1 inline-flex flex-auto flex-wrap overflow-hidden text-nowrap text-sm">
-          <Badge label="JIRA" />
-          <Badge label="Confluence" />
-          <Badge label="Bitbucket" />
-          <Badge label="Miro" />
-          <Badge label="Google Workspace" />
-        </div>
-        <p></p>
+        <p className="text-sm">{period}</p>
+        <p className="font-bold">{company}</p>
+        <h4 className="text-xl font-semibold uppercase text-[#324DD2] md:text-2xl">{jobTitle}</h4>
+        {projects.map((project, index) => (
+          <div key={index}>
+            <p className="font-semibold">{project.title}</p>
+            <span className="text-sm">{project.description}</span>
+            <ul className="ml-5 list-disc space-y-1 text-sm">
+              {project.responsibilities.map((responsibility, index) => (
+                <li key={index}>{responsibility}</li>
+              ))}
+            </ul>
+            <div className="my-1 mb-4 inline-flex flex-auto flex-wrap overflow-hidden text-nowrap text-sm">
+              {project.techStack.map((tech, index) => (
+                <Badge label={tech} key={index} />
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </article>
   );
--- app/ui/sections/Experience.tsx
+++ app/ui/sections/Experience.tsx
@@ -7,23 +7,183 @@ import ExperienceCard from "../components/ExperienceCard";
 type Props = {};
 
 export default function Experience({}: Props) {
+  const jobs = [
+    {
+      period: "Sep 2021 - Jun 2023",
+      company: "ICEO - Venture Builder",
+      jobTitle: "VP of Frontend Development",
+      projects: [
+        {
+          title: "Norion • Fintech/Blockchain • Frontend Department",
+          description:
+            "Platform for building complete tokenisation ecosystems. Offers the issuance and sale of token for any asset.",
+          responsibilities: [
+            "Prepared frontend architecture for complex client-tailored microecosystems concept - domain driven multirepo with components library.",
+            "Worked as a developer during initial phase of the project - implemented components tests etc.",
+            "Conducted 100+ technical interviews in order to build experienced, varied team with proper skills and mindset for project.",
+          ],
+          techStack: [
+            "React",
+            "MUI",
+            "TS",
+            "Redux Toolkit",
+            "Storybook",
+            "RTL",
+            "Cypress",
+            "Zeroheight",
+            "Design system",
+          ],
+        },
+        {
+          title: "ICEO • Management",
+          description: "Member of company management.",
+          responsibilities: [
+            "Frontend representative in company management level meetings.",
+            "Managed and growth frontend team from business side - structure, evaluations, negotiations, procedures, integrations, onboardings, JIRA.",
+            "Responsible for company strategy, development and growth.",
+          ],
+          techStack: ["JIRA", "Confluence", "Bitbucket", "Miro", "Google Workspace"],
+        },
+      ],
+    },
+    {
+      period: "Jun 2020 - Aug 2021",
+      company: "ICEO - Venture Builder • Frontend Department",
+      jobTitle: "Senior Frontend Developer",
+      projects: [
+        {
+          title: "DoxyChain • Legaltech/Blockchain",
+          description:
+            "A solution for managing document flow and secure electronic signing based on blockchain technology.",
+          responsibilities: [
+            "Prepared frontend architecture as a Project Lead for DoxyChain.",
+            "Developed React components with maintaining high code quality.",
+            "Developed frontend views in accordance with the provided UI.",
+            "Code review, testing and reducing tech debt.",
+            "Cooperated with teams from other departments - backend, design etc.",
+          ],
+          techStack: ["React", "MUI", "TypeScript", "Redux", "RTL"],
+        },
+        {
+          title: "MediHouse • Medtech",
+          description:
+            "A telemedicine system (5 products) that enables online consultations with doctors and diagnostics through the support of modern technology.",
+          responsibilities: [
+            "Developed React components with maintaining high code quality.",
+            "Developed frontend views in accordance with the provided UI.",
+            "Code review, testing.",
+            "Developed WebRTC based video chat application with mediasoup SFU.",
+          ],
+          techStack: ["Next.js", "React", "JavaScript", "MUI", "PrimeReact", "WebRTC", "RTL"],
+        },
+      ],
+    },
+    {
+      period: "Mar 2018 - May 2020",
+      company: "ICEO - Venture Builder • Frontend Department",
+      jobTitle: "Frontend Developer",
+      projects: [
+        {
+          title: "AutoInvestor • Fintech/Blockchain",
+          description: "A tool for automatic trading on cryptocurrency exchanges.",
+          responsibilities: [
+            "Prepared frontend architecture as a Project Lead for AutoInvestor.",
+            "Prepared UX/UI for the project.",
+            "Developed React components with maintaining high code quality.",
+            "Developed frontend views in accordance with the provided UI.",
+            "Code review, testing and reducing tech debt.",
+            "Cooperated with teams from other departments - backend, design etc.",
+          ],
+          techStack: ["Angular", "RxJS", "PrimeNG", "Jasmine"],
+        },
+        {
+          title: "BitBayBot • Fintech/Blockchain",
+          description: "A tool for automatic trading on cryptocurrency exchanges.",
+          responsibilities: [
+            "Next version of AutoInvestor with new features crafted for better experience.",
+            "User driven approach and UI consistent with cryptocurrency exchange.",
+          ],
+          techStack: ["React", "MobX", "Material-UI", "Jest"],
+        },
+      ],
+    },
+    {
+      period: "May 2016 - Feb 2018",
+      company: "Data System Group • IT Department",
+      jobTitle: "Frontend Developer",
+      projects: [
+        {
+          title: "DS Locate • Fleet management",
+          description: "GPS localization system for fleet management.",
+          responsibilities: [
+            "Implemented brand new app based on UI project with new stack.",
+            "Maintained and developed new features on legacy app.",
+            "Cooperated with teams from other departments - backend, design etc.",
+          ],
+          techStack: ["Angular", "RxJS", "PrimeNG", "Jasmine"],
+        },
+        {
+          title: "Workflow • Company management",
+          description:
+            "Managing of company documents workflow - from online based contracts to assembly cards within one synced ecosystem.",
+          responsibilities: [
+            "Developed frontend views in accordance with the provided UI.",
+            "Cooperated with teams from other departments - backend, design etc.",
+          ],
+          techStack: ["Angular", "PrimeNG"],
+        },
+      ],
+    },
+    {
+      period: "Nov 2014 - May 2016",
+      company: "LUMEL S.A. • R&D Department",
+      jobTitle: "Production Assistant",
+      projects: [
+        {
+          title: "Labels",
+          description:
+            "System for managing and printing nameplates for devices manufactured in assembly plant.",
+          responsibilities: [
+            "Developed software from scratch.",
+            "System management and end-user support.",
+            "Role included both backend and frontend side.",
+          ],
+          techStack: ["Qt", "JavaScript", "jQuery"],
+        },
+        {
+          title: "Various projects",
+          description: "Supporting the work of the electronics production department.",
+          responsibilities: [
+            "Implemented calibration software.",
+            "Implemented software for HMI panels.",
+            "Implemented support for new devices in comapny config software.",
+          ],
+          techStack: ["Qt", "JavaScript", "HMI"],
+        },
+      ],
+    },
+  ];
   return (
     <motion.div
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1 }}
-      className="relative mx-auto flex h-screen max-w-7xl  flex-col items-center justify-evenly overflow-hidden px-4 text-center md:flex-row md:px-10 md:text-left"
+      className="relative mx-auto flex h-screen max-w-[2000px]  flex-col items-center justify-evenly overflow-hidden px-4 text-center md:flex-row md:px-10 md:text-left"
     >
       <h3 className="invisible absolute top-24 mr-[-16px] text-2xl font-semibold uppercase tracking-[16px] text-white  md:visible">
         Experience
       </h3>
 
       <div className="scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#324DD2]/80 mt-12 flex w-full snap-x snap-mandatory space-x-5 overflow-x-scroll p-10">
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
+        {jobs.map((job, index) => (
+          <ExperienceCard
+            key={index}
+            period={job.period}
+            company={job.company}
+            jobTitle={job.jobTitle}
+            projects={job.projects}
+          />
+        ))}
       </div>
     </motion.div>
   );
